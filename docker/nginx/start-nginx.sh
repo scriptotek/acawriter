711172499b8304266261d03b93526edebfb0f29c
Dan Michael O. Heggø <danmichaelo@gmail.com>
2020-12-14 13:48:33 +0100

    Fix start-nginx.sh

--- docker/nginx/start-nginx.sh
+++ docker/nginx/start-nginx.sh
@@ -7,11 +7,13 @@ CERTBOT_DIR=/etc/letsencrypt
 
 echo "Starting nginx using $NGINX_SITE.conf"
 
-if [ -f $CERTBOT_DIR/options-ssl-nginx.conf ]; then 
+if [ ! -f $CERTBOT_DIR/options-ssl-nginx.conf ]; then
+  echo Fetching options-ssl-nginx.conf
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > $CERTBOT_DIR/options-ssl-nginx.conf
 fi
 
-if [ -f $CERTBOT_DIR/ssl-dhparams.pem ]; then 
+if [ ! -f $CERTBOT_DIR/ssl-dhparams.pem ]; then
+  echo Fetching ssl-dhparams.pem
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > $CERTBOT_DIR/ssl-dhparams.pem
 fi
 
